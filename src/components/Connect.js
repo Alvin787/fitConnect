bfc7199dce8835eb156b2c6ab1f7e145e2a2a053
Alvin787 <alvinhalo4@gmail.com>
2021-09-19 04:25:59 -0400

    created connection page with all users

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -1,9 +1,46 @@
 import React, { useEffect, useState } from "react";
 import Card from "./UI/Card";
 import axios from "axios";
+import styles from "./Profile.module.css";
 
-const Connect = () =>{
+const Connect = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
  
-}
 
-export default Connect
+
+  useEffect(() => {
+    axios
+      .get("https://api.chatengine.io/users", {
+        headers: { "private-key": process.env.REACT_APP_CHAT_ENGINE_KEY },
+      })
+
+      .then((resp) => {
+        setUsers(resp.data);
+        setLoading(false);
+      })
+
+      .catch(console.error);
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <p>loading</p>
+      ) : (
+        <div>
+          {users.map((users) => {
+            return (
+              <Card className={styles.main}>
+                <img src={users.avatar}></img>
+                <p>{users.username}</p>
+              </Card>
+            );
+          })}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Connect;
