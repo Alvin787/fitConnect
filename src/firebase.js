217504b2e16a1af6b2d1fbef3cc1e2faaa5a7b05
Jeffery Zhan <jefferyzhan84@gmail.com>
2021-09-19 01:12:19 -0400

    fixed duplicate firebase

--- src/firebase.js
+++ src/firebase.js
@@ -24,7 +24,6 @@ export const auth = firebase.initializeApp( {
     appId: "1:301271393586:web:8f3c5ce169eac86714632b"
   }).auth();
 
-  const initFirebase = firebase.initializeApp(auth)
   const db = initFirebase.firestore();
 
   export default db;
