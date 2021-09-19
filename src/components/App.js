da5eadbf724587b00670ade7293553d5b688f50b
Alvin787 <alvinhalo4@gmail.com>
2021-09-18 20:42:12 -0400

    home page

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import { AuthProvider } from "../contexts/AuthContext";
 
 import Chats from "./Chats"
 import Login from "./Login"
+import Home from "./Home"
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Switch>
             <Route path="/chats" component={Chats} />
             <Route path="/" component={Login} />
+            <Route path="/connect" component={Home}/>
           </Switch>
         </AuthProvider>
       </Router>
---
+++ src/components/Home.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect} from 'react';
+import { useHistory } from 'react-router-dom';
+import { ChatEngine } from 'react-chat-engine';
+import { auth } from '../firebase';
+
+import { useAuth } from '../contexts/AuthContext';
+import axios from 'axios';
+
+
+const Home = () => {
+/*     const history = useHistory();
+
+    const handleLogout = async () => {
+        await auth.signOut();
+
+        history.push('/');
+    } */
+    return(
+        <div className="chats-page">
+            <div className="nav-bar">
+                <div className="logo-tab">
+                    fitConnect
+                </div>
+{/*                 <div onClick={handleLogout}className="logout-tab">
+                    Logout
+                </div> */}
+            </div>
+        </div>
+    )
+};
+
+export default Home;
