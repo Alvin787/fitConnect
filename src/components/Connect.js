b0308a74e06b1efd4eb5f8b14c36274297c3ba90
Jeffery Zhan <jefferyzhan84@gmail.com>
2021-09-19 01:06:35 -0400

    wtf is wrong with npm

---
+++ src/components/Connect.js
@@ -0,0 +1,37 @@
+import React, {useEffect, useState } from 'react';
+import Post from 'post';
+import db from 'firebase';
+
+import { Container, Flex, Spinner, VStack } from '@chakra-ui/core';
+
+const App = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        //Hook to handle the inital fetching of posts
+
+        db.collection("posts")
+        .orderBy("createdAt", "desc")
+        .get()
+        .then((querySnapshot) => {
+            const data = querySnapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            }));
+
+            setPosts(data);
+        });
+    }, []);
+
+    return (
+        <>
+            <Container maxW="md" centerContent p={8}>
+                <VStack spacing={8} w="100%">
+                    {posts.map((post) => (
+                        <Post post={post} key={post.id} />
+                    ))}
+                </VStack>
+            </Container>
+        </>
+    );
+};
